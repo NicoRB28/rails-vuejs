54799dccbf435a435da89a62b84901de1e68e988
NicoRB28 <nicolasrodriguezboschi@gmail.com>
2020-07-19 23:47:37 -0300

    second commit

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -33,11 +33,67 @@
 
 
  import Vue from 'vue/dist/vue.esm'
- import App from '../app.vue'
+ //import Task from '../task.vue'
+// import task from 'task-components'
 
- document.addEventListener('DOMContentLoaded', () => {
-   const app = new Vue({
-     el: '#hello',
+document.addEventListener('DOMContentLoaded', () => {
+
+Vue.component('task',{
+    props:['task'],
+    template:
+        `
+           <div class="ui segment task" v-bind:class="task.completed ? 'done' : 'todo'"> 
+                <div class="ui grid">
+                    <div class="row">
+                        <div class="left floated equal wide column">
+                            <div class="ui checkbox">
+                                <input type="checkbox" name="task" v-on:click="toggleDone($event, task.id)" :checked="task.completed">
+                                <label>{{task.name}}<span class="description">{{task.description}}</span></label>
+                            </div>
+                        <div>
+                        <div class="right floated equal wide column">
+                            <i class="icon pencil" alt="Edit" v-on:click="editTask($event, task.id)"></i>
+                            <i class="icon trash" alt="Delete" v-on:click="deleteTask($event, task.id)"></i>
+                        </div>
+                    </div>
+             </div>
+        `, 
+   methods:{
+        findTask: function(id){
+            return app.tasks.find(item => item.id == id);
+        },
+       toggleDone: function(event, id){
+           event.stopImmediatePropagation();
+           let task = this.findTask(id);
+           if(task){
+               task.completed = !task.completed;
+               app.message = `Task $${id} updated.`
+           }
+       },
+       editTask: function(event, id){
+            app.action = 'edit';
+           let task = this.findTask(id);
+           if(task){
+               app.task = {
+                   id: task.id,
+                   name: task.name,
+                   description: task.description,
+                   completed: task.completed
+               };
+             }
+       },
+       deleteTask: function(event, id){
+            event.stopImmediatePropagation();
+           let taskIndex = app.tasks.findIndex(item => item.id == id);
+           if(taskIndex >= 0){
+                app.$delete(app.tasks, taskIndex);
+               app.message = `Task ${id} deleted.`
+           }
+       }
+   }
+})
+    new Vue({
+     el: '#app',
      data: {
         tasks: [
             {   
@@ -113,7 +169,7 @@
             
         }
     },
-    components: { App }
+    //components: {task}
    });
  })
 
